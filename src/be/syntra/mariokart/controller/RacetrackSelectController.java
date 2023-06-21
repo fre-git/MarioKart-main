7e7d17a399400695d789244ea45775fad7a16763
Frederic Gielen <Frederic-Gielen@cursist.syntrapxl.be>
2023-06-21 19:33:07 +0200

    map2 name fix

--- src/be/syntra/mariokart/controller/RacetrackSelectController.java
+++ src/be/syntra/mariokart/controller/RacetrackSelectController.java
@@ -62,6 +62,6 @@ public class RacetrackSelectController implements IController {
     @FXML
     void selectTrack2() {
         audio.playAudioSelect();
-        map = new Map("File:resources/images/Backgroundpixell3Colors2.png");
+        map = new Map("File:resources/images/Backgroundpixell3Colors3.png");
     }
 }
